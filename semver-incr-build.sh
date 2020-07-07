e4f73eff6a253438b16db8fdfc1ecbd02f29afcb
Robert van den Breemen <robert@vandenbreemen.net>
2020-07-07 21:45:04 +0200

    Update semver-incr-build.sh

--- semver-incr-build.sh
+++ semver-incr-build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #
+# Copyright (c) 2020 Robert van den Breemen - released under MIT license - see the end of this file
+#
 # This script auto increment a header file, that can be included in your projects
 # Using the format as described by Semantic Version 2.0 format (Read more https://semver.org/)
 # Note: this script does not implement pre-release tagging at this point
@@ -91,3 +93,25 @@ TIMESTAMP=
 echo $VERSION
 
 exit 1
+
+# MIT License
+#
+# Copyright (c) 2020 Robert van den Breemen
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
