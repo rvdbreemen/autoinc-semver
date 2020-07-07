401b5cb4a2ca00a5a700edefc734cb0a4f38153b
Robert van den Breemen <robert@vandenbreemen.net>
2020-07-07 21:42:39 +0200

    Update semver-incr-build.bat
    Added MIT license

--- semver-incr-build.bat
+++ semver-incr-build.bat
@@ -1,4 +1,7 @@
 @echo off
+rem 
+rem Copyright (c) 2020 Robert van den Breemen - released under MIT license -
+rem 
 rem This script auto increment a header file, that can be included in your projects
 rem Using the format as described by Semantic Version 2.0 format (Read more https://semver.org/)
 rem Note: this script does not implement pre-release tagging at this point
@@ -89,3 +92,29 @@ set PATCH=
 set BUILD=
 set VERSION=
 set TIMESTAMP=
+
+goto :eof
+
+rem =================================================================================
+rem MIT License
+rem 
+rem Copyright (c) 2020 Robert van den Breemen
+rem 
+rem Permission is hereby granted, free of charge, to any person obtaining a copy
+rem of this software and associated documentation files (the "Software"), to deal
+rem in the Software without restriction, including without limitation the rights
+rem to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+rem copies of the Software, and to permit persons to whom the Software is
+rem furnished to do so, subject to the following conditions:
+rem 
+rem The above copyright notice and this permission notice shall be included in all
+rem copies or substantial portions of the Software.
+rem 
+rem THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+rem IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+rem FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+rem AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+rem LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+rem OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+rem SOFTWARE.
+rem =================================================================================
